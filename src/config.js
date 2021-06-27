e9ae28478efd28036f24d05feab27ad47fcee4a3
Omar J <okcomputer2393@gmail.com>
2021-06-26 19:46:10 -0500

    update linkedin link

--- src/config.js
+++ src/config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/edson-omar-juarez/',
     },
   ],
 
